2d49c6140eaabd5bf2bf517e5573a7d70c454f5e
MasayaMatsushita <s183367@stn.nagaokaut.ac.jp>
2022-05-31 09:01:29 +0900

    jsonファイルの有無で処理をどうするか

--- my_calendar.py
+++ my_calendar.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import os
 from operator import ge
 import tkinter
 import datetime as dt
@@ -50,7 +51,8 @@ def set_cal(cal, cal_cell, year, month):
         cal_cell[i1]["text"] = str1
         
     event_day_list = []
-    if ping("google.com") != False:
+    #pingが通るか and googleapiに接続するjsonファイルが存在するか
+    if ping("google.com") != False and os.path.isfile(os.path.dirname(os.path.abspath(__file__))+'/raspi-calendar.json'):
         import get_googlecal
         event_day_list = get_googlecal.get_event_day(year, month)
     list_data = []
